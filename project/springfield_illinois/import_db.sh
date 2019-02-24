30dcfb9ddc73c87b2697aa2b321d788161482fab
Myeong Lee <deeper.myeong.lee@gmail.com>
2019-02-24 16:45:50 +0000

    db import sh

---
+++ project/springfield_illinois/import_db.sh
@@ -0,0 +1,5 @@
+mongoimport --db scribe_api_development --collection projects --file  projects.json
+mongoimport --db scribe_api_development --collection subjects --file  subjects.json
+mongoimport --db scribe_api_development --collection groups --file  groups.json
+mongoimport --db scribe_api_development --collection classifications --file classifications.json
+mongoimport --db scribe_api_development --collection final_subject_sets --file final_subject_sets.json
